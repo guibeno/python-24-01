96786dcc53f67aa7f0c580b01352027d63d84c06
guibeno <167104831+guibeno@users.noreply.github.com>
2024-04-25 11:05:24 -0300

    matrix.py

---
+++ matrix.py
@@ -0,0 +1,11 @@
+a=[
+    [1, 2],
+    [3, 4],
+]
+
+k = 2
+for linha in range(len(a)):
+    for coluna in range(len(a[linha])):
+        a[linha][coluna]
+        a[linha][coluna] * k
+    print(a[linha])
